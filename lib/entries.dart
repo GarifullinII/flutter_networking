cf3d36368ebb20b1b4545fe20e7efc80d542d124
GarifullinII <ildaridrisovich@gmail.com>
2021-12-17 21:10:35 +0400

    added Manual JSON serialization

--- lib/entries.dart
+++ lib/entries.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class EntriesList {
   List<Entry> entries;
   EntriesList({required this.entries});
 
-  factory EntriesList.fromJson (Map<String, dynamic> json) {
+  factory EntriesList.fromJson(Map<String, dynamic> json) {
     var EntriesJson = json['entries'] as List;
 
-    List<Entry>entriesList = EntriesJson.map((i) => Entry.fromJson(i)).toList();
+    List<Entry> entriesList =
+        EntriesJson.map((i) => Entry.fromJson(i)).toList();
 
     return EntriesList(entries: entriesList);
   }
@@ -23,5 +28,16 @@ class Entry {
         title: json['title'] as String,
         key: json['key'] as String,
         revision: json['revision'] as num);
+  }
+}
+
+Future<EntriesList> getEntriesList() async {
+  const url = 'https://openlibrary.org/authors/OL23919A/works.json';
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    return EntriesList.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Error: ${response.reasonPhrase}');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_networking/entries.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
@@ -19,46 +20,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<EntriesList>? entryList;
+
   @override
   void initState() {
     super.initState();
-    loadData();
+    entryList = getEntriesList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Networking'),
+        title: Text('Manual JSON serialization'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: FutureBuilder<EntriesList>(
+        future: entryList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.entries.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text('${snapshot.data!.entries[index].title}'),
+                      subtitle: Text(
+                          '${snapshot.data!.entries[index].key}, ${snapshot.data!.entries[index].revision}'),
+                      isThreeLine: true,
+                    ),
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Text('Error');
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
 
-Future<http.Response> getData() async {
-  const url = 'https://openlibrary.org/authors/OL23919A/works.json';
-  return await http.get(
-    Uri.parse(url),
-    // headers: {
-    //   "Access-Control-Allow-Origin": 'true',
-    // },
-  );
-  // ответ с типом Response в виде объекта Future
-  // метод синхронный, добавляю await - асинхронный
-}
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     loadData();
+//   }
 
-void loadData() {
-  getData().then((response) {
-    // обрабатываю полученные данные с сервера
-    // для этого проверяю response на статус кода
-    if (response.statusCode == 200) {
-      print(response.body);
-    } else {
-      print(response.statusCode);
-    }
-  }).catchError((error) {
-    debugPrint(error.toString());
-  }); // обрабатываю общую ошибку вызываю метод catchError(onError)
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Networking'),
+//         centerTitle: true,
+//       ),
+//       body: Container(),
+//     );
+//   }
+// }
+
+// Future<http.Response> getData() async {
+//   const url = 'https://openlibrary.org/authors/OL23919A/works.json';
+//   return await http.get(
+//     Uri.parse(url),
+//     // headers: {
+//     //   "Access-Control-Allow-Origin": 'true',
+//     // },
+//   );
+//   // ответ с типом Response в виде объекта Future
+//   // метод синхронный, добавляю await - асинхронный
+// }
+
+// void loadData() {
+//   getData().then((response) {
+//     // обрабатываю полученные данные с сервера
+//     // для этого проверяю response на статус кода
+//     if (response.statusCode == 200) {
+//       print(response.body);
+//     } else {
+//       print(response.statusCode);
+//     }
+//   }).catchError((error) {
+//     debugPrint(error.toString());
+//   }); // обрабатываю общую ошибку вызываю метод catchError(onError)
+// }
